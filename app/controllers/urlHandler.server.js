a332f5f6180a27811a2687239177191434cb8ba4
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-12-11 16:50:09 -0800

    refactor of server into many apps

--- app/controllers/urlHandler.server.js
+++ app/controllers/urlHandler.server.js
@@ -1,18 +1,15 @@
+'use strict';
 function urlHandler(db) {
-    'use strict';
     /* jshint validthis:true */
     
     const isURL = require('validator/lib/isURL');
 
-    // create and/or access the collection
-    var urlcoll = db.collection('urlcoll');
-
     var url_to_shorten;
 
-    this.urlServiceGet = function(req, res){
+    this.urlServiceGet = function(req, res, next){
         url_to_shorten = req.params.url_to_shorten;
        
-        // url query parameters have been stripped; put them back
+        // url query string have been stripped by expressjs; put them back
         // (i.e. we want www.example.com?a=2 and www.example.com?a=3 to be
         // two separate entries)
         if (!isEmpty(req.query)) {
@@ -20,36 +17,41 @@ function urlHandler(db) {
                 .replace(/:/g, '=').replace(/["{}]/g, '').replace(/,/g, '&')
         }
         
-        if (validUrl(url_to_shorten)) {
-            shortenURL(req, res)
-        } else {
-            res.json({"error": url_to_shorten + "is not a valid url"})
-        }
+        // pass the reconstructed url_to_shorten in body
+        req.body.url_to_shorten = url_to_shorten;
+
+        next()
+
     }
     
-    this.urlServicePost = function(req, res){
-        url_to_shorten = req.body.url_to_shorten;
-        if (validUrl(url_to_shorten)) {
-            shortenURL(req, res)
-        } else {
-            res.json({"error": url_to_shorten + "is not a valid url"})
-        }
-    }
-    
-    function validUrl(url) {
+    this.validate = function(req, res, next){
         // check if url is in a valid form
+        
+        url_to_shorten = req.body.url_to_shorten;
+        
+        console.log('url_to_shorten validé: ' + url_to_shorten)
+
+        // options for isURL: we want check only http(s) protocol
         var isurl_options = {protocols:  ['http', 'https'] }
-        return isURL(url, isurl_options)
+        if (!isURL(url_to_shorten, isurl_options)) {
+            res.json({"error": url_to_shorten + " is not a valid url"})
+        } else {
+            next()
+        }
+
     }
        
-    function shortenURL(req, res) {
-   
+    this.shortenURL = function(req, res, next) {
+    
+        // create and/or access the collection; attach it to req for further use
+        req.urlcoll = db.collection('urlcoll');
+        
         // prepend the http protocol if not present
         if (!url_to_shorten.startsWith('http')) {
             url_to_shorten = 'http://' + url_to_shorten
         }
     
-        urlcoll.findOne({'longurl': url_to_shorten}, {}, (err, doc) => {
+        req.urlcoll.findOne({'longurl': url_to_shorten}, {}, (err, doc) => {
             if (err) return console.err('error in shortenURL' + err);
             
             if (doc !== null) {
@@ -57,56 +59,49 @@ function urlHandler(db) {
                 res.send('urllong existant: ' + doc.longurl + 
                         ',  racourci: ' + doc.shorturl);
             } else { 
-                // url_to_shorten doesn't exist yet
-                // we get the last shorturl name entered 
-                // from which we will generate a new one
-                urlcoll.find({}, {'shorturl':1, _id:0})
-                    .limit(1).sort({$natural: -1}).next( (err, doc) => {
-                        if (err) console.error('err in insertNewUrl ' + err);
-                        // generate a new short from the last one used
-                        var new_short = makeShortUrl(doc.shorturl);
-                            // and create a new document in the db
-                            urlcoll.insert({
-                                'longurl': url_to_shorten, 
-                                'shorturl': new_short
-                            }, (err, doc) => {
-                                if (err) return console.log(err);
-                                res.send('nouveau urllong: ' + url_to_shorten + 
-                                        ', nouveau racourci: ' + new_short);
-                            })
-                    })
+                // url_to_shorten doesn't exist yet. So we get the last shorturl
+                // in the db, from which we will generate a new one
+                next()
+                
+                // urlcoll.find({}, {'shorturl':1, _id:0}).limit(1)
+                //     .sort({$natural: -1}).next( (err, doc) => {
+                //         if (err) console.error('err in insertNewUrl ' + err);
+                //         // generate a new short from the last one used
+                //         var new_short = makeShortUrl(doc.shorturl);
+                //             // and create a new document in the db
+                //             urlcoll.insert({
+                //                 'longurl': url_to_shorten, 
+                //                 'shorturl': new_short
+                //             }, (err, doc) => {
+                //                 if (err) return console.log(err);
+                //                 res.send('nouveau urllong: ' + url_to_shorten + 
+                //                         ', nouveau racourci: ' + new_short);
+                //             })
+                //     })
             }
-        
         })
     }
 
-    
-    /*
-    }, writeResult
-    
-    function writeResult(err, doc) {
-        if (err) return console.error(err);
-        res.send('nouveau urllong: ' + url_to_shorten + 
-                ', nouveau racourci: ' + new_short);
-    }
-    */
-   
-    /*
-    function insertNewUrl(err, doc) {
-        if (err) console.error('err in insertNewUrl ' + err);
-        // generate a new short from the last one used
-        var new_short = makeShortUrl(doc.shorturl)
-            // and create a new document in the db
-            urlcoll.insert({
-                'longurl': url_to_shorten, 
-                'shorturl': new_short
-            }, (err, doc) => {
-                if (err) return console.log(err);
-                res.send('nouveau urllong: ' + url_to_shorten + 
-                        ', nouveau racourci: ' + new_short);
+    this.createNewShort = function(req, res) {
+        // var urlcoll = req.urlcoll;
+
+        req.urlcoll.find({}, {'shorturl':1, _id:0}).limit(1)
+            .sort({$natural: -1}).next( (err, doc) => {
+                if (err) console.error('err in insertNewUrl ' + err);
+                // generate a new short from the last one used
+                var new_short = makeShortUrl(doc.shorturl);
+                // and create a new document in the db
+                req.urlcoll.insert({
+                    'longurl': url_to_shorten, 
+                    'shorturl': new_short
+                }, (err, doc) => {
+                    if (err) return console.log(err);
+                    res.send('nouveau urllong: ' + url_to_shorten + 
+                            ', nouveau racourci: ' + new_short);
+                })
             })
     }
-    */
+
     
     function isEmpty(obj) {
         return Object.keys(obj).length === 0;
--- app/routes/index.js
+++ app/routes/index.js
@@ -7,12 +7,25 @@ module.exports = function(app, db) {
     // note: some google chrome extensions cause GET requests to be sent twice!
 
     var urlHandler = new UrlHandler(db)
+   
+    var {validate, shortenURL, createNewShort} = urlHandler
+    var processURL = [validate, shortenURL, createNewShort]
+
+    // app.route('/').get((req, res) => {
+    //     res.sendFile(process.cwd() + '/public/index.html')
+    // }).post(urlHandler.validate, urlHandler.shortenURL)
     
     app.route('/').get((req, res) => {
         res.sendFile(process.cwd() + '/public/index.html')
-    })
+    }).post(processURL)
+
+    // app.route('/new/:url_to_shorten(*)')
+    //     .get(urlHandler.urlServiceGet, urlHandler.validate, urlHandler.shortenURL)
     
-    app.route('/new/:url_to_shorten(*)')
-        .get(urlHandler.urlServiceGet)
-        .post(urlHandler.urlServicePost)
+    app.route('/new/:url_to_shorten(*)').get(urlHandler.urlServiceGet, [processURL])
+
+    app.route('*').get((req, res)=> {
+        res.end('invalide')
+    })
+        
 }
